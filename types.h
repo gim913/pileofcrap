12c4b3bc8861486032e08593a84ed93d8d37c7f2
GiM <gim913@gmail.com>
2011-06-04 12:52:18 +0200

    initial types.h

---
+++ types.h
@@ -0,0 +1,15 @@
+#ifndef GIM_TYPES_H
+#define GIM_TYPES_H
+
+#include <stdint.h>
+
+typedef int8_t          e_byte;
+typedef uint8_t         e_ubyte;
+typedef int16_t         e_short;
+typedef uint16_t        e_ushort;
+typedef int32_t         e_int;
+typedef uint32_t        e_uint;
+typedef int64_t         e_long;
+typedef uint64_t        e_ulong;
+
+#endif
