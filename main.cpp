279de6e7d0795c978d83d13b785acf5dcfbcf103
GiM <gim913@gmail.com>
2011-06-04 14:59:46 +0200

    some more tests

--- main.cpp
+++ main.cpp
@@ -21,6 +21,39 @@ namespace {
 		search.init(b);
 		ASSERT_EQ(b.ptr, search.getPattern().ptr);
 	}
+	
+	struct TestMississippi : public ::testing::Test {
+		TestMississippi() : txt("Mississippi", sizeof("Mississippi")-1) {}
+		POD::ConstBuffer txt;
+	};
+	TEST_F(TestMississippi, TestBegin) {
+		POD::ConstBuffer p("Mis", sizeof("Mis")-1);
+		POD::ConstBuffer r = Search::pattern(p, txt);
+		ASSERT_EQ(txt.ptr, r.ptr);
+	}
+	
+	TEST_F(TestMississippi, TestMiddle1) {
+		POD::ConstBuffer p("sis", sizeof("sis")-1);
+		POD::ConstBuffer r = Search::pattern(p, txt);
+		ASSERT_EQ( (txt.ptr + 3), r.ptr);
+	}
+	
+	TEST_F(TestMississippi, TestMiddle2) {
+		POD::ConstBuffer p("ssi", sizeof("ssi")-1);
+		POD::ConstBuffer r = Search::pattern(p, txt);
+		ASSERT_EQ( (txt.ptr + 2), r.ptr);
+	}
+	
+	TEST_F(TestMississippi, TestMiddle3) {
+		POD::ConstBuffer p("sip", sizeof("sip")-1);
+		POD::ConstBuffer r = Search::pattern(p, txt);
+		ASSERT_EQ( (txt.ptr + 6), r.ptr);
+	}
+	TEST_F(TestMississippi, TestEnd) {
+		POD::ConstBuffer p("ppi", sizeof("ppi")-1);
+		POD::ConstBuffer r = Search::pattern(p, txt);
+		ASSERT_EQ( (txt.ptr + 8), r.ptr);
+	}
 }
 
 int main(int argc, char **argv) {
