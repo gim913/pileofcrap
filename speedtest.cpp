7a0bd91641dd40261a8458afbacba40a01cbc63f
GiM <gim913@gmail.com>
2011-06-14 19:52:56 +0200

    fix the code, hadn't g++ 4.5 earlier

--- speedtest.cpp
+++ speedtest.cpp
@@ -12,19 +12,15 @@ public:
 	RandomMemBlock(size_t size) {
 		std::uniform_int_distribution<int> byteDist(0, 255);
 		std::mt19937 mersenneTwister;
-		
 		mem.resize(size);
-		
-		//auto generator = std::bind(byteDist, mersenneTwister);
-		std::generate(mem.begin(), mem.end(), []()->char{ return byteDist(mersenneTwister); } );
+		std::generate(mem.begin(), mem.end(), [&]()->char{ return byteDist(mersenneTwister); } );
 	}
 	
 	~RandomMemBlock() {
-		delete [] mem;
 	}
 	
 	POD::ConstBuffer getBuffer() {
-		return POD::ConstBuffer(mem, size);
+		return POD::ConstBuffer(&(mem[0]), mem.size());
 	}
 private:
 	std::vector<char> mem;
@@ -34,8 +30,6 @@ int main(int argc, char **argv)
 {
 	RandomMemBlock rnd(0x400 * 0x400 * 20);
 	auto t = rnd.getBuffer();
-	
-	
 	
 	return 0;
 }
