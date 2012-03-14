95c5a481f69d63c2f71ee322b85caa966339dd69
GiM <gim913@gmail.com>
2012-03-14 19:25:10 +0100

    salsa20exp1 working

--- salsa20/main.cpp
+++ salsa20/main.cpp
@@ -108,6 +108,28 @@ struct Salsa20
 		salsa20(tempIn, salsaExpOutput);
 #undef LENDIAN
 	}
+
+	static void salsa20exp1(uint8_t k[16], uint8_t n[16], uint8_t (&salsaExpOutput)[64]) {
+#define LENDIAN(a,b,c,d) (((d)<<24)|((c)<<16)|((b)<<8)|(a))
+		static const uint32_t t0 = LENDIAN(101, 120, 112, 97);
+		static const uint32_t t1 = LENDIAN(110, 100,  32, 49);
+		static const uint32_t t2 = LENDIAN( 54,  45,  98, 121);
+		static const uint32_t t3 = LENDIAN(116, 101,  32, 107);
+
+		uint8_t tempIn[64];
+		uint32_t *tempIn32 = (uint32_t*)tempIn;
+
+		tempIn32[0] = t0;
+		memcpy(tempIn + 4*1, k, 16);
+		tempIn32[5] = t1;
+		memcpy(tempIn + 4*2 + 16, n, 16);
+		tempIn32[10] = t2;
+		memcpy(tempIn + 4*3 + 32, k, 16);
+		tempIn32[15] = t3;
+
+		salsa20(tempIn, salsaExpOutput);
+#undef LENDIAN
+	}
 };
 
 void check(int num, bool isSuccess) {
@@ -315,19 +337,37 @@ void testSalsaExp2(int& testNo) {
 	uint8_t n[16]  = { 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116 };
 	uint8_t salsaExp2Output[64];
 
-	Salsa20::salsa20exp2(k0, k1, n, salsaExp2Output);
-	static const uint8_t correct[64] = {
-		69, 37, 68, 39, 41, 15,107,193,255,139,122, 6,170,233,217, 98,
-		89,144,182,106, 21, 51,200, 65,239, 49,222, 34,215,114, 40,126,
-		104,197, 7,225,197,153, 31, 2,102, 78, 76,176, 84,245,246,184,
-		177,160,133,130, 6, 72,149,119,192,195,132,236,234,103,246, 74
-	};
-	for (int i = 0; i < 64; ++i) {
-		if (salsaExp2Output[i] != correct[i]) {
-			check(testNo, false);
+	{
+		Salsa20::salsa20exp2(k0, k1, n, salsaExp2Output);
+		static const uint8_t correct[64] = {
+			69, 37, 68, 39, 41, 15,107,193,255,139,122, 6,170,233,217, 98,
+			89,144,182,106, 21, 51,200, 65,239, 49,222, 34,215,114, 40,126,
+			104,197, 7,225,197,153, 31, 2,102, 78, 76,176, 84,245,246,184,
+			177,160,133,130, 6, 72,149,119,192,195,132,236,234,103,246, 74
+		};
+		for (int i = 0; i < 64; ++i) {
+			if (salsaExp2Output[i] != correct[i]) {
+				check(testNo, false);
+			}
 		}
+		check(testNo++, true);
 	}
-	check(testNo++, true);
+
+	{
+		Salsa20::salsa20exp1(k0, n, salsaExp2Output);
+		static const uint8_t correct[64] = {
+			39,173, 46,248, 30,200, 82, 17, 48, 67,254,239, 37, 18, 13,247,
+			241,200, 61,144, 10, 55, 50,185, 6, 47,246,253,143, 86,187,225,
+			134, 85,110,246,161,163, 43,235,231, 94,171, 51,145,214,112, 29,
+			14,232, 5, 16,151,140,183,141,171, 9,122,181,104,182,177,193
+		};
+		for (int i = 0; i < 64; ++i) {
+			if (salsaExp2Output[i] != correct[i]) {
+				check(testNo, false);
+			}
+		}
+		check(testNo++, true);
+	}
 }
 int main()
 {
